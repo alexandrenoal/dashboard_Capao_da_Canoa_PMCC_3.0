70df959c8769f2e27adfadba86c6b4f0c814953b
Alexandre Noal <73001363+alexandrenoal@users.noreply.github.com>
2026-05-28 13:41:20 -0300

    Create sidebar.css

---
+++ css/sidebar.css
@@ -0,0 +1,10 @@
+.logo {
+    display: flex;       /* Ativa o modo flexbox */
+    align-items: center; /* Alinha a imagem e o texto verticalmente pelo centro */
+    gap: 10px;           /* Cria um espaço entre a imagem e o nome */
+}
+
+/* Opcional: Garante que a imagem não tenha margens indesejadas */
+.logo-img img {
+    display: block;
+}
